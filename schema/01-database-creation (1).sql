e5d299d57e8732bcfccf0278f406f367c481f7c2
Sai purushotham Alahari <145708039+Sai-Purushotham@users.noreply.github.com>
2025-12-14 02:44:31 +0530

    Delete schema directory

--- schema/01-database-creation (1).sql
+++
@@ -1,8 +0,0 @@
--- ============================================================================
--- SMART INVENTORY & SALES MANAGEMENT SYSTEM
--- File 1: Database Creation
--- ============================================================================
-
-DROP DATABASE IF EXISTS inventory_sales_system;
-CREATE DATABASE inventory_sales_system;
-USE inventory_sales_system;
